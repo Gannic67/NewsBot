e1bd647d4e726fce0e2e482c9018e3ad7dc80e69
Gannic67 <gannicus182@mail.ru>
2024-02-25 21:05:27 +0300

    Доделал кнопку вернуться в главное меню

--- main.py
+++ main.py
@@ -34,7 +34,7 @@ def welcome(message):
     button1 = telebot.types.KeyboardButton("Новости Смоленск")
     button2 = telebot.types.KeyboardButton("Новости Москва")
     keyboard.add(button2, button1)
-    bot.send_message(chat_id,'Добро пожаловать!', reply_markup=keyboard)
+    bot.send_message(chat_id,'Привет!Спасибо, что читаешь новости ', reply_markup=keyboard)
 
 
 @bot.message_handler(func=lambda message: message.text == 'Новости Москва')
@@ -43,7 +43,9 @@ def smol(message):
     keyboard = telebot.types.ReplyKeyboardMarkup(resize_keyboard=True)
     button1 = telebot.types.KeyboardButton("Погода")
     button2 = telebot.types.KeyboardButton("Благоустройство города")
+    back = telebot.types.KeyboardButton("Вернуться в меню")
     keyboard.add(button1, button2)
+    keyboard.add(back)
     bot.send_message(chat_id, 'Выбирите пункт', reply_markup=keyboard)
 
 
@@ -55,18 +57,16 @@ def moscow(message):
     button1 = telebot.types.KeyboardButton("Ремонт дорог")
     button2 = telebot.types.KeyboardButton("Дтп")
     button3 = telebot.types.KeyboardButton("Кино-Афиша")
+    back = telebot.types.KeyboardButton("Вернуться в меню")
     keyboard.add(button1, button2)
-    keyboard.add(button3)
+    keyboard.add(back, button3)
     bot.send_message(chat_id,'Выбирите пункт', reply_markup=keyboard)
 
+@bot.message_handler(func=lambda message: message.text == 'Вернуться в меню')
+def back_to_menu(message):
+    welcome(message)
 
-@bot.message_handler(func=lambda message: message.text == 'Вернуться в главное меню')
-def exit(message):
-    chat_id = message.chat.id
-    keyboard = telebot.types.ReplyKeyboardMarkup(resize_keyboard=True)
-    back = telebot.types.KeyboardButton("Вернуться в главное меню")
-    keyboard.add(back)
-    bot.send_message(chat_id, 'Выбирите пункт', reply_markup=keyboard)
+
 
 
 if __name__ == '__main__':
