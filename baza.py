989e9f7bde6e3b823b4d65a2635115d034d3919f
maks67 <gannicus182@mail.ru>
2024-03-16 19:09:29 +0300

    Создал пробную таблицу sql для теста телеграмм бота

--- baza.py
+++ baza.py
@@ -1,8 +1,19 @@
 import pymysql
 
+
 connection = pymysql.connect(host='147.45.105.54',
                              user='gen_user',
                              password='mfohmdu%E3$b\k',
-                             database='default_db',
+                             database='News',
                              port=3306)
+
+
+with connection:
+    cursor = connection.cursor()
+    cursor.execute('SELECT * FROM Smolensk')
+
+    rows = cursor.fetchall()
+
+    for line in rows:
+        print(line)
 
