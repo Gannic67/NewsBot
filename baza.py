eb2763ec329bc8db94ddbd04fd12364be0fc8146
maks67 <gannicus182@mail.ru>
2024-04-18 20:27:18 +0300

    Доделал базу данных для новостей

--- baza.py
+++ baza.py
@@ -1,12 +1,4 @@
-import pymysql
-
-
-connection = pymysql.connect(host='147.45.105.54',
-                             user='gen_user',
-                             password='|2?xzWwGa+KF\B',
-                             database='News',
-                             port=3306)
-
+from SQL import connection
 
 # with connection:
 #     cursor = connection.cursor()
@@ -18,25 +10,44 @@ connection = pymysql.connect(host='147.45.105.54',
 #         print(line)
 
 def get():
-
     cursor = connection.cursor()
     cursor.execute('SELECT * FROM MOSCOW')
-
     rows = cursor.fetchall()
     a =[]
     for line in rows:
         a.append(f'{line[1]}\n\n, {line[2]}\n\n, {line[3]}\n\n')
     return a
 
+def getsmoscow():
+    cursor = connection.cursor()
+    cursor.execute('SELECT * FROM mascowDTP')
+    rows = cursor.fetchall()
+    q =[]
+    for line in rows:
+        q.append(f'{line[1]}\n\n, {line[2]}\n\n, {line[3]}\n\n')
+    return q
+
 
 def smol():
     cursor = connection.cursor()
     cursor.execute('SELECT * FROM Smolensk')
-
     rows = cursor.fetchall()
     b = []
     for line in rows:
         b.append(f'{line[1]}\n\n, {line[2]}\n\n, {line[3]}\n\n')
     return b
+
+def smols():
+    cursor = connection.cursor()
+    cursor.execute('SELECT * FROM SmolenskDTP')
+    rows = cursor.fetchall()
+    q = []
+    for line in rows:
+        q.append(f'{line[1]}\n\n, {line[2]}\n\n, {line[3]}\n\n')
+    return q
+
+
+
+
 
 
